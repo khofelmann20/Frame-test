4f24f922323e06e63a4781826e74bdb3c6db9e9f
khofelmann20 <43548275+khofelmann20@users.noreply.github.com>
2019-01-11 15:04:04 -0800

    plotting points using buttons
    Using JFRame to recreate Morley's python code. Creating a visualizer for plotting points. Using Java Frame

--- src/main/java/pantryvisualizer/Visualizer.java
+++ src/main/java/pantryvisualizer/Visualizer.java
@@ -3,83 +3,69 @@ package pantryvisualizer;
 import java.util.ArrayList;
 import java.awt.Point;
 import java.util.Arrays;
-
 import pantryvisualizer.math.Translation2d;
+import java.awt.Frame;
+import javax.swing.*;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.FontMetrics;
+import java.awt.Font;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+import java.awt.image.BufferedImage;
 
-/**
- * A path generation tool for Pure Persuit. Given some coordinates, this program 
- * will interpolate and smooth the path, and display the generated path as well as the robot's bumpers
- * on the field. 
- */
-public class Visualizer{
-  public static void main(String[] args) {
-    System.out.println("Hello there!");
-  }
-  ArrayList<Integer> topLeft = new ArrayList<Integer>(Arrays.asList(217,40));
-  ArrayList<Integer> bottomRight = new ArrayList<Integer>(Arrays.asList(1372, 615));
-  ArrayList<Integer> fieldSize = new ArrayList<Integer>(Arrays.asList(54, 27));
-  ArrayList<Integer> fieldSizeInPixels = new ArrayList<Integer>(Arrays.asList((bottomRight.get(0)-topLeft.get(0)), (bottomRight.get(1)-topLeft.get(1))));
-  ArrayList<Integer> imageSize = new ArrayList<Integer>(Arrays.asList(1592, 676));
+import javax.swing.JComponent;
 
-  ArrayList<Translation2d> waypoints = new ArrayList<Translation2d>(Array.;
-  Point spacing;
 
-  // public double[][] getSmoothPath(){}
+public class Visualizer extends JFrame{ 
+    JFrame frame = new JFrame(); 
   
-  /**
-   * Convert a x,y double in field coordinates into pixels on the field image
-   * @param 
-   */
-  public Point convertToPixels(Point feet) {
-    Point imageCoordinates = new Point(feet.getX() / fieldSize.get(0) * fieldSizeInPixels.get(0) + topLeft.get(0), feet.getY() / fieldSize.get(1) * fieldSizeInPixels.get(1) + topLeft.get(1));
-    return new Point(x,y);
-  }
-
-  public ArrayList<Point> injectPoints(ArrayList<Point> waypoints, int numPoints) {
-    ArrayList<Point> linePoints = new ArrayList<Point>();
-    for (int point : waypoints){
-      tx = (waypoints.get(point).getX() - waypoints.get(point-1).getX());
-      ty = (waypoints.get(point).getY() - waypoints.get(point-1).getY());
-
-      linePoints.add(waypoints.get(point));
-      numPoints--;
-      spacing = new Point(Math.abs(tx/numPoints),Math.abs(ty/numPoints));
-
-      for (int lp; lp<=numPoints-1; lp++){
-        linePoints.add(new Point((linePoints.get(0).getX() + spacing.getX()*(lp+1)),(linePoints.get(0).getY()+spacing.getY()*(lp+1))));
-      }
-      if(waypoints.get(point)==waypoints.get(waypoints.length()-1)){
-        linePoints.add(waypoints.get(point));
-      }
+    Visualizer(){ 
+        setTitle("This is the title");
+ 
+        // creates instance of JButton 
+       JButton button1 = new JButton(" "); 
+  
+        button1.setBounds(200, 150, 5, 5); 
+          
+        // setting close operation 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+  
+        // adds button in JFrame 
+        frame.add(button1); 
+    
+        frame.setSize(600, 600); // sets 500 width and 600 height 
+           
+        frame.setLayout(null); // uses no layout managers
+          
+        frame.setVisible(true); // makes the frame visible 
+    } 
+    public void paint (Graphics g){
+        Graphics2D g2 = (Graphics2D) g; 
+        int x = 150;
+        int y = 150;
+        int rectwidth = 10;
+        int rectheight = 50;
+        g2.draw(new Ellipse2D.Double(x, y, rectwidth, rectheight));
     }
-  return linepoints;
-  }
+    public static void main(String[] args) { 
+        new Visualizer();
+        // Points p = new Points();
+
+        // p.init();
+        // p.draw(10,10);
+    } 
+   
 
 
-    public double[][] smoother(double[][] path, double weightData, double weightSmooth, double tolerance)
-    {
-  
-      //copy array
-      ArrayList<Point> newPath = doubleArrayCopy(path);
-  
-      double change = tolerance;
-      while(change >= tolerance)
-      {
-        change = 0.0;
-        for(int i=1; i<path.length-1; i++)
-          for(int j=0; j<path[i].length; j++)
-          {
-            double aux = newPath[i][j];
-            newPath[i][j] += weightData * (path[i][j] - newPath[i][j]) + weightSmooth * (newPath[i-1][j] + newPath[i+1][j] - (2.0 * newPath[i][j]));
-            change += Math.abs(aux - newPath[i][j]);	
-          }					
-      }
-  
-      return newPath;
-  }
+} 
 
-  // public Point distanceBetweenPoints ()
-  // Distances Between Points
-  // distance at point i = distance at point (i − 1) + distance_f ormula(point i, point (i − 1))
 
-}
+
